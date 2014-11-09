e2c9c3101c0dd6e29e4f0377091163c7da840b3c
Taikai Sebastian Hirose <sebastian@cloudpack.jp>
2014-11-10 02:05:54 +0900

    first commit

---
+++ js/jq.ui.ext.calender.js
@@ -0,0 +1,210 @@
+﻿( function( $, undefined ) {
+    var plugname = 'ext_calender';
+    var year , month;
+    var startDate;
+    var elements;
+    var separator = false;
+    var _click = null;
+
+    $.fn[plugname] = function(params){
+        return new Class(this, $.extend(this._defaultStyle , params, {}));
+    }
+    var Class = function(elem , params){
+        elements = elem;
+        this.elements = elem;
+        this.elements.data(plugname, this)
+        year = params['year'];
+        month = params['month'];
+        separator = params['separator'];
+        if (year == undefined)
+            year = (new Date()).getFullYear();
+        if (month == undefined)
+            month = (new Date()).getMonth() + 1;
+        if (params['click'] != undefined)
+            _click = params['click']
+        makeCalender(year , month)
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             prevMonth
+    /// @brief          change previous month
+    /// @param[in]      (none)
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.prevMonth = function() {
+        month--;
+        Class.prototype.Month(month);
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             nextMonth
+    /// @brief          change next month
+    /// @param[in]      (none)
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.nextMonth = function() {
+        month++;
+        Class.prototype.Month(month);
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             Month
+    /// @brief          set monthly number
+    /// @param[in]      number of month
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.Month = function(mm) {
+        if (mm == undefined)
+            return month;
+        if (month == mm) return;
+        month = mm;
+        makeCalender(year , month);
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             prevYear
+    /// @brief          change previous year
+    /// @param[in]      (none)
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.prevYear = function() {
+        year--;
+        Class.prototype.Year(year);
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             nextYear
+    /// @brief          change next year
+    /// @param[in]      (none)
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.nextYear = function() {
+        year++;
+        Class.prototype.Year(year);
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             Year
+    /// @brief          set year number
+    /// @param[in]      number of year
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.Year = function(yy) {
+        if (yy == undefined)
+            return year;
+        if (year == yy) return;
+        year = yy;
+        makeCalender(year , month);
+    }
+    
+    Class.prototype.setYearMonth = function(yy , mm) {
+        year = yy;
+        month = mm;
+        makeCalender(year , month);
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             getDayElement
+    /// @brief          day element
+    /// @details        get element of inner html in 'div' tag
+    /// @param[in]      number of day
+    /// @param[out]     (none)
+    /// @return         jQuery selected object
+    /// -----------------------------------------------------------------------
+    Class.prototype.getDayElement = function(day) {
+        var id = '#' + year + convertNum(month,2) + convertNum(day,2);
+        var e = $(id);
+        return e;
+    }
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    //
+    // The following functions are private functions.
+    //
+    //
+    ///////////////////////////////////////////////////////////////////////////
+    function makeCalender(year , month){
+        elements.html('');
+        var wk = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+        var headcls = ['ext-cal-sun-header','ext-cal-day-header','ext-cal-day-header','ext-cal-day-header','ext-cal-day-header','ext-cal-day-header','ext-cal-sat-header'];
+        startDate = new Date(year , month - 1, 1);
+        endDate = new  Date(year , month , 1);
+        endDate.setDate(endDate.getDate() - 1);
+        endDate.setDate(endDate.getDate() + (6 - endDate.getDay()));
+        startDate.setDate(startDate.getDate() - startDate.getDay());
+        // make header
+        var thead = $('<thead>');
+        for (var w = 0 ; w < 7 ; w++){
+            thead.append($('<th>').text(wk[w]).addClass('ext-cal-header').addClass(headcls[w]));
+        }
+        
+        var tbody = $('<tbody>');
+        startSr = getSerial(startDate);
+        endSr = getSerial(endDate);
+        var dy = new Date()
+        var tr =$('<tr>');
+        for (var w = startSr ; w <= endSr ; w+=86400000){
+            dy.setTime(w);
+            console.log(dy.toLocaleString());
+            var id = dy.getFullYear() + convertNum(dy.getMonth()+1,2) + convertNum(dy.getDate(),2)
+            var td = $('<td>').addClass('ext-cal-day')
+                .append($('<div>').addClass('ext-cal-datenumber').text(dy.getDate()))
+            if (separator) {
+                td.append($('<hr>').addClass('ext-cal-separator'));
+            }
+            td.append($('<div>').attr({id:id})
+              .addClass('ext-cal-string'))
+              .attr({date_serial:w, year:dy.getFullYear(),month:dy.getMonth(),day:dy.getDate()});
+            if (dy.getMonth() + 1 != month){
+                td.addClass('ext-cal-transparent');
+            }
+            if (dy.getDay() == 0){
+                tr = $('<tr>');
+                td.addClass('ext-cal-sun');
+                tr.append(td);
+            } else if (dy.getDay() == 6) {
+                td.addClass('ext-cal-sat');
+                tr.append(td);
+                tbody.append(tr);
+            } else {
+                td.addClass('ext-cal-week')
+                tr.append(td);
+            }
+        }
+        elements.addClass('ext-cal').append(thead).append(tbody);
+        
+        elements.ext_tables({
+            click : function(params) {
+                var serial = $(params['cell']).attr('date_serial');
+                var dt = new Date();
+                dt.setTime(serial);
+                params['date'] = dt;
+                _click($(params['cell']) , params);
+            } ,
+        });
+        function getSerial(dt){
+            var yy = dt.getFullYear();
+            var MM = dt.getMonth();
+            var dd = dt.getDate();
+            var hh = dt.getHours();
+            var mm = dt.getMinutes();
+            var srl = Date.UTC(yy,MM,dd,hh,mm,0);
+            return srl;
+        }
+    }
+    function convertNum(num, figures) {
+        var str = String(num);
+        if (str.length > figures){
+            str2 = new String();
+            for (var i=str.length-1;i >= 0;i--) {
+                str2 = str.charAt(i) + str2;
+                if (str2.length == figures)
+                    break;
+            }
+            str = str2;
+        }
+        while (str.length < figures) {
+            str = "0"+str;
+        }
+        return str;
+    }
+})(jQuery);
---
+++ js/jq.ui.ext.table.js
@@ -0,0 +1,291 @@
+﻿( function( $, undefined ) {
+    var plugname = 'ext_tables';
+    var isedited = false;
+    var isEdit = false;
+    var isSort = false;
+    var clickDisableRows = new Array();
+    var tableListData = new Array();
+    var sortOrderFlag = new Array();
+    var editFlag = new Array();
+    var tableId = '';
+
+    var _default = {
+                        searchBox : false ,
+                        isEdit : false ,
+                        isSort : false ,
+                        click : null ,
+                        dblclick : null ,
+                        headclick : null ,
+                        change : null ,
+                        
+                        search : null ,
+                        sorted : null ,
+                   };
+    $.fn[plugname] = function(params){
+        return new Class(this, $.extend(this._defaultStyle , params, {}));
+    }
+    var Class = function(elem , params){
+        tableId = elem[0].id;
+        this.elements = elem;
+        this.elements.data(plugname, this)
+        _default = $.extend(_default, params);
+        getListData();
+        setClickMethod();
+        if (_default['searchBox'])
+            setSearchRows(elem);
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             descendingOrder
+    /// @brief          descending order
+    /// @details        This method performs rearrangement in descending order in the specified column. 
+    /// @param[in]      colIndex    A colmun is specified numerically. 
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.descendingOrder = function(colIndex) {
+        sortOrderFlag[colIndex] = -1;
+        sortList(colIndex , sortOrderFlag[colIndex]) ;
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             ascendingOrder
+    /// @brief          ascending order 
+    /// @details        This method performs rearrangement in ascending order in the specified column. 
+    /// @param[in]      colIndex    A colmun is specified numerically. 
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.ascendingOrder = function(colIndex) {
+        sortOrderFlag[colIndex] = 1;
+        sortList(colIndex , sortOrderFlag[colIndex]) ;
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             toggleOrder
+    /// @brief          Order tggle
+    /// @details        This method replaces an order of the specified column. 
+    /// @param[in]      colIndex    A colmun is specified numerically. 
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.toggleOrder = function(colIndex) {
+        sortOrderFlag[colIndex] *= -1;
+        sortList(colIndex , sortOrderFlag[colIndex]) ;
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             search(keywords)
+    /// @brief          Data is narrowed down.  
+    /// @details        Only the data in which this function suits the keyword specified from the table displays. 
+    /// @n              Each element of an argument [keywords] corresponds to each row of a table. 
+    /// @param[in]      keywords    specifies in arrangement. 
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.search = function(keywords) {
+        searchData(keywords);
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             search(keywords)
+    /// @brief          Data is narrowed down.  
+    /// @details        This method searches a table with the value of a search box. 
+    /// @param[in]      (none)
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    Class.prototype.inputSearch = function() {
+        var keywords = new Array();
+        var inputs = $('#'+ tableId +' thead tr > th > input');
+        inputs.each(function(i){
+            keywords.push($(this).val());
+        });
+        searchData(keywords);
+    }
+    Class.prototype.setEditEnable = function(row , col , flag) {
+        editFlag[row][col] = flag;
+    }
+    Class.prototype.getEditEnable = function(row , col) {
+        return editFlag[row][col];
+    }
+    Class.prototype.setEditFlags = function(flagArray) {
+        editFlag = flagArray;
+    }
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    //
+    // The following functions are private functions.
+    //
+    //
+    ///////////////////////////////////////////////////////////////////////////
+    /// -----------------------------------------------------------------------
+    /// @brief          assigns Click-Event to Table-Tag. 
+    /// @details        This function is a function which assigns a click event.
+    /// @param[in]      (none)
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    function setClickMethod(){
+        var rows = $("#" + tableId)[0].rows;
+        var isHeader = true;
+        // header click
+        if (isSort){
+            $('#' + tableId + ' thead tr:has(th)').each(function(row) {
+                $('#' + tableId + ' thead tr:eq(' + row + ') th').each(function(col) {
+                    var objCell = $(this);
+                    var text = $(this).text();
+                    $(this).click(function(e) {
+                        _default.headclick({column:$(this)[0].cellIndex,text:$(this).text()});
+                        Class.prototype.toggleOrder($(this)[0].cellIndex);
+                    }).mouseover(function(e){
+                        $(this).css('cursor','pointer');
+                    });
+                });
+            });
+        }
+        // cell click
+        $('#' + tableId + ' tbody tr').each(function(row) {
+            $('#' + tableId + ' tbody tr:eq(' + row + ') > *').each(function(col) {
+                var text = $(this).text();
+                if ($.inArray(row,clickDisableRows)) {
+                    if (this.tagName.toUpperCase() == 'TD') {
+                        var param = {'row':row , 'column':col ,'text':text, 'cell':$(this)};
+                        $(this).click(function(e){
+                            _default.click(param);
+                            if (isEdit && editFlag[row][col]){
+                                cellEdit($(this));
+                            }
+                        })
+                        .change(function(e){
+                            _default.change(param);
+                        })
+                        .mouseover(function(e){
+                            $(this).css('cursor','pointer');
+                        });
+                    }
+                }
+            });
+        });
+    }
+    function setSearchRows(objTable){
+        var tr = $('<tr>');
+        var tableLength = objTable.children().length;
+        var thead = $('#'+ objTable[0].id+' thead');
+        var thtr = $('#'+ objTable[0].id+' tr:has(th)');
+        var cols =  $('#'+ objTable[0].id+ ' thead tr:first').children().length;
+        // no thead-tag
+        if (tableLength == 1) {
+            thead = $('<thead>');
+            if (thtr.length != 0)
+                thead.append(thtr);
+            objTable.prepend(thead);
+            cols = thtr.children().length;
+        } else if (tableLength == 0) {
+            thead = $('<thead>');
+            objTable.prepend(thead)
+        }
+        for (var i = 0 ; i < cols ;i++)
+            tr.append($('<th>').css({backgroundColor:'aliceblue'}).append(
+            $('<input>').css({width:'98%',border:0,backgroundColor:'transparent'}).keydown(function(e){
+                if (e.keyCode == 13) {
+                    Class.prototype.inputSearch();
+                }
+            })));
+        thead.append(tr);
+        clickDisableRows.push(tr.index());
+    }
+    /// -----------------------------------------------------------------------
+    /// @fn             searchData(keywords)
+    /// @brief          Data is narrowed down.  
+    /// @details        Only the data in which this function suits the keyword specified from the table displays. 
+    /// @n              Each element of an argument [keywords] corresponds to each row of a table. 
+    /// @param[in]      keywords    specifies in arrangement. 
+    /// @param[out]     (none)
+    /// @return         (none)
+    /// -----------------------------------------------------------------------
+    function searchData(keywords){
+        var tbody  = $('#'+ tableId +' tbody tr > *');
+        // All the data is displayed.
+        $('#'+ tableId+' tbody tr').each(function(i) {
+            $('#'+ tableId+' tbody tr:eq(' + i + ')').css('visibility' , 'visible');
+        });
+        if (keywords.length == 0){
+            return;
+        }
+        // The data which is not applied to conditions is made undisplayed. 
+        tbody.each(function(i){
+            var cell = this;
+            var keyword = keywords[$(this).index()];
+            var text = $(this).text();
+            var row = $(this)[0].parentElement.rowIndex;
+            if (keyword != '' && keyword != null){
+                if (text.indexOf(keyword) == -1)
+                    $(this.parentElement).css('visibility' , 'collapse');
+            }
+        });
+        // [search] is called.
+        _default.search({keywords:keywords});
+    }
+    function getListData(){
+        tableListData = new Array();
+        sortOrderFlag = new Array();
+        editFlag = new Array();
+        $('#'+ tableId+' tbody tr').each(function(row) {
+            var rowData = new Array();
+            var editData = new Array();
+            $('#'+ tableId+' tbody tr:eq(' + row + ') > *').each(function(col){
+                rowData[col] = $(this).text();
+                editData[col] = true;
+            });
+            tableListData[row] = rowData;
+            editFlag[row] = editData;
+        });
+        $('#'+ tableId+' tbody tr:first td').each(function(i){
+            sortOrderFlag.push(1);
+        });
+    }
+    function cellEdit(cell){
+        if (isedited) return;
+        isedited = true;
+        var input = $('<input>');
+        var text = cell.text();
+        var row = cell[0].parentElement.rowIndex;
+        var col = cell[0].cellIndex;
+        var backgound = cell.css('backgroundColor');
+        cell.css({backgroundColor:'aliceblue'}).text('').append(input);
+        input.attr({type:'text',id:'dateselector'}).val(text)
+            .css({width:'99%',height:cell.height() ,backgroundColor:'transparent',border:'0px'})
+            .keydown(function(e) {
+                if (e.keyCode == 13) {
+                    if (input.val() != text)
+                        text = input.val();
+                    cell.text(text).css('backgroundColor' , backgound);
+                    tableListData[row][col] = text;
+                    input.remove();
+                    isedited = false;
+                }
+            })
+            .blur(function(e) {
+                cell.text(text).css('backgroundColor' , backgound);
+                input.remove();
+                isedited = false;
+            }).focus();
+    }
+    function sortList(col , flag) {
+        if (! isSort) return
+        tableListData = sort(tableListData,col,flag);
+        $.each(tableListData,function(row, data){
+            $.each(data , function(col,value){
+                $($('#' + tableId + ' tbody tr:eq('+ row +')')[0].cells[col]).text(value);
+            });
+        });
+        _default.sorted({colmun:col , order:flag});
+        Class.prototype.inputSearch();
+    }
+    function sort(sqs,col,order){
+        sqs.sort(function(a,b){
+            var as = a[col].toString();
+            var bs = b[col].toString();
+            if (as > bs) return 1 * order; 
+            if (as < bs) return -1 * order; 
+            return 0; 
+         });
+         return(sqs);
+    }
+})(jQuery);
